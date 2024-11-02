7dc3b9010de57a086004b873f413a012cf61a2b4
William Moura <williamonte@gmail.com>
2024-11-02 20:47:53 -0300

    Feat: EmailJS API

--- js/script.js
+++ js/script.js
@@ -2,9 +2,7 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header'); 
     const backgroundImage = document.querySelector('#background-image'); 
-
     const backgroundHeight = backgroundImage.offsetHeight;
-
     if (window.scrollY > backgroundHeight) {
         header.classList.add('solid-background'); 
     } else {
@@ -12,17 +10,20 @@ window.addEventListener('scroll', function() {
     }
 });
 
+// Swiper JS
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: { el: '.swiper-pagination', clickable: true },
+    navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+});
+
 // Função para buscar usuários e preencher a seção de testemunhos
 fetch("https://randomuser.me/api/?results=3")
     .then(response => response.json())
     .then(data => {
         const testimonialsContainer = document.getElementById('testimonials-container');
-
-        // Limpa o contêiner antes de adicionar novos testemunhos
-        testimonialsContainer.innerHTML = '';
-
+        testimonialsContainer.innerHTML = ''; // Limpa o contêiner antes de adicionar novos testemunhos
         data.results.forEach(user => {
-            // Cria um elemento para cada testemunho
             const testimonialDiv = document.createElement('div');
             testimonialDiv.classList.add('testimonial');
             testimonialDiv.innerHTML = `
@@ -32,4 +33,34 @@ fetch("https://randomuser.me/api/?results=3")
             `;
             testimonialsContainer.appendChild(testimonialDiv);
         });
-    });
+    }
+);
+
+/* EmailJS */
+function sendMail() {
+    let params = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_sp4lbal";
+    const templateID = "template_lgkewko";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your response sent successfully!!");
+        })
+        .catch(err => {
+            alert("Erro ao enviar mensagem: " + JSON.stringify(err));
+            console.log(err);
+        }
+    );
+}
+
